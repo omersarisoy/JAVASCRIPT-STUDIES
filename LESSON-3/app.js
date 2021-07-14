6a937e16d2f1cb11a25341ecd4ba35e66104f58f
omersarisoy <omersarisoy1993@gmail.com>
2021-07-14 11:15:40 +0300

    DAILY JAVASCRIPT-STUDIES DURING CLARUSWAY

---
+++ LESSON-3/app.js
@@ -0,0 +1,49 @@
+// console.log("I m from outside")
+// console.log("I m from otherside") 
+var a ;
+console.log( typeof a)
+var a = 5;
+console.log(typeof a)
+var b = 1.67
+console.log(typeof b)
+var c = -5
+console.log(typeof c)
+
+var f = 999999999999999
+var g = 9999999999999999
+console.log(f)
+console.log(g)
+
+var h =0.1 + 0.2;
+console.log(h)
+
+var userName = "Ömer"
+var a = `Merhaba ${userName}`
+console.log(a)
+
+console.log("%c Hello Full Stack!","background:#840A7F; color:#fff")
+
+var veys = 8 > 4;
+console.log(veys)
+
+console.log(8>4);
+
+console.log(typeof 4==4);
+console.log(typeof (4==4));
+
+var x = Boolean(0)
+console.log(x);
+
+var x = Boolean("0")
+console.log(x);
+
+var x = Boolean("")
+console.log(x);
+
+var x = Boolean(" ")
+console.log(x);
+
+var x = Boolean(null)
+console.log(x);
+
+console.log(typeof null);
