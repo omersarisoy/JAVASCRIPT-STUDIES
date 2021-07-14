cfbd75303958b62546afd943794914238e904292
omersarisoy <omersarisoy1993@gmail.com>
2021-07-14 14:43:04 +0300

    DAILY JAVASCRIPT-STUDIES DURING CLARUSWAY

---
+++ LESSON-4/app.js
@@ -0,0 +1,158 @@
+console.log("This is lesson-4 <<< Operators<<<");
+// var a = 3;
+// var b = a--;
+// console.log(b);
+// console.log(a);
+
+// var x = 3;
+// var y = --x;
+// console.log(y);
+// console.log(x); 
+
+//--------------------------------------------------------------
+
+// var a;
+// var aa;
+// var aaa;
+
+// function add (a,b){
+//     console.log(a+b)
+// }
+
+// add(3,4)
+
+//---------------------------------------------------------------
+// let a;
+// let aa;
+// let aaa;
+
+// let abc = function add (a,b){
+//     console.log(a+b)
+// }
+
+// abc(3,4)
+
+//-----------------------------------------------------------------
+
+// let a = 5
+// console.log(a);
+
+// {
+//     let a = 8
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// -------------------------------------------------------------------
+
+// var a = 3;
+// let b = a--    // bu satırda a = 3
+
+// console.log({a});    // bu satırda a = 2
+// console.log({b});
+
+// var c = 3;
+// let d = --c     // bu satırda c = 2
+
+// console.log({c});   // bu satırda c = 2
+// console.log({d});
+
+// ----------------------------------------------------------------------
+
+// var b = 5;
+
+// // console.log(++b);  // b=6
+// // console.log(b++);  // b=5
+// // console.log(b);    // b=6
+
+// // var a = ++b   // b= b+1,  a=b
+// // console.log({b});  // b=6
+// // console.log({a});  // a=6
+
+
+// var a = b++   // a=b , b = b+1
+// console.log({b});  // b=6
+// console.log({a});  // a=5
+
+//----------------------------------------------------------------------
+
+// Assignment Operators
+
+// var a = 6;
+// var b = 2;
+​
+// console.log(a=b, {a},{b})
+// console.log(a+=b, {a},{b}) 
+// console.log(a-=b, {a},{b})
+// console.log(a/=b, {a},{b})
+// console.log(a*=b, {a},{b})
+// console.log(a%=b, {a},{b})
+​
+// var a = 2;
+// var b = '2';
+​
+// console.log('strict equality',a === b) //false
+// console.log('equality',a == b) //true
+​
+​
+// var a = 2;
+// var b = '2';
+​
+// console.log('strict inequality',a !== b) // true
+// console.log('inequality',a != b) //false
+​
+// let age = 14;
+​
+// console.log(age >= 14) //true
+// console.log(age > 14) //false
+​
+​
+// console.log(false == false) //true
+// console.log(Boolean(0) == false) //true
+// console.log(Boolean('0') == false) //false
+// console.log(0 == false) // true
+// console.log('0' == false) //true
+​
+// console.log(typeof null) //object
+// console.log(null == false) //false
+// console.log(null == true) //false
+// console.log(null+3) //3
+// console.log(null == 0) //false
+// console.log(Boolean(null) == false) //true
+// console.log(Boolean(null) == Boolean(0)) //true
+​
+// console.log(typeof NaN)
+​
+//isNaN
+​
+// Kullanıcıdan alınan veri sayı mmı değil mi?
+// var a ;
+​
+// while(isNaN(a)){
+//     a = prompt("Enter a Number")
+//     a = Number(a)
+//     console.log(a, typeof a)
+// }
+​
+// console.log(015+025) //34 = 13 + 21
+// console.log(15+025) //36 = 15 + 21
+// console.log(015+25) //38 = 13 + 25
+// console.log(15+25) //40
+​
+// console.log(0015+0025)
+​
+// 0 1 2 3 4 5 6 7
+​
+// console.log(9 < 8) //false
+// console.log( 9 < 8 < 7 ) // true
+// console.log(false < 7) //true
+​
+// console.log('Clarusway'> 'clarusway') //false
+// console.log('A'.charCodeAt())
+// console.log('a'.charCodeAt())
+
+​
+​
+​
+​
