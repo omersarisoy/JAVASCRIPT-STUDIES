dd69040c1775510eba0d463a45a7d40fefc2501a
omersarisoy <omersarisoy1993@gmail.com>
2021-07-25 17:40:05 +0300

    DAILY JAVASCRIPT-STUDIES DURING CLARUSWAY

---
+++ LESSON-9/arrays.js
@@ -0,0 +1,180 @@
+// const cars = ['Audi', 'Opel','Fiat']
+// console.log(typeof cars);
+// console.log(cars);
+​
+// const numbers = new Array(2,10)
+// console.log(numbers);
+​
+// const numbers2 = new Array(10)
+// console.log(numbers2);
+​
+// const cars = ['Audi', 'Opel','Fiat']
+// console.log('lenght of car :',cars.length);
+​
+// const cars = ['Audi', 'Opel','Fiat'];
+// const cars2 = ['BMW','Ferrari'];
+​
+// const cars3 = cars.concat(cars2, 'Mercedes',2021);
+// console.log(cars);
+// console.log(cars2);
+// console.log(cars3);
+​
+// console.log(typeof(cars+cars2));
+// console.log(cars+cars2);
+​
+//Sort()
+/* const points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort());
+// points.sort(function(a, b){return a - b});
+​
+console.log(points.sort(function(a, b){return a - b})); */
+​
+/* Sonuç negatif ise a, b'den önce sıralanır.
+​
+Sonuç pozitifse, b, a'dan önce sıralanır.
+​
+Sonuç 0 ise, iki değerin sıralama düzeninde herhangi bir değişiklik yapılmaz. */
+​
+/*
+//Pop & push
+const cars = ['Audi', 'Opel','Fiat'];
+​
+cars.push('Renault')
+​
+console.log(cars);
+​
+cars.pop() // () parametre almaz her zaman sonuncuyu siler
+​
+console.log(cars);
+ */
+​
+//Shift Unshift
+​
+​
+/* const cars = ['Audi', 'Opel','Fiat'];
+​
+// const deleted = cars.shift()
+​
+// console.log(deleted)
+​
+// console.log(cars);
+​
+const added = cars.unshift('VW')
+console.log(cars);
+console.log('Geri dönen',added); */
+
+// const cars = ['Audi', 'Opel','Fiat']
+// console.log(typeof cars);
+// console.log(cars);
+​
+// const numbers = new Array(2,10)
+// console.log(numbers);
+​
+// const numbers2 = new Array(10)
+// console.log(numbers2);
+​
+// const cars = ['Audi', 'Opel','Fiat']
+// console.log('lenght of car :',cars.length);
+​
+// const cars = ['Audi', 'Opel','Fiat'];
+// const cars2 = ['BMW','Ferrari'];
+​
+// const cars3 = cars.concat(cars2, 'Mercedes',2021);
+// console.log(cars);
+// console.log(cars2);
+// console.log(cars3);
+​
+// console.log(typeof(cars+cars2));
+// console.log(cars+cars2);
+​
+//Sort()
+/* const points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort());
+// points.sort(function(a, b){return a - b});
+​
+console.log(points.sort(function(a, b){return a - b})); */
+​
+/* Sonuç negatif ise a, b'den önce sıralanır.
+​
+Sonuç pozitifse, b, a'dan önce sıralanır.
+​
+Sonuç 0 ise, iki değerin sıralama düzeninde herhangi bir değişiklik yapılmaz. */
+​
+/*
+//Pop & push
+const cars = ['Audi', 'Opel','Fiat'];
+​
+cars.push('Renault')
+​
+console.log(cars);
+​
+cars.pop() // () parametre almaz her zaman sonuncuyu siler
+​
+console.log(cars);
+ */
+​
+//Shift Unshift
+​
+​
+// const cars = ['Audi', 'Opel','Fiat'];
+​
+// // const deleted = cars.shift()
+​
+// // console.log(deleted)
+​
+// // console.log(cars);
+​
+// const added = cars.unshift('VW')
+// console.log(cars);
+// console.log('Geri dönen',added);
+​
+/* let cars = ['Audi', 'Opel','Fiat'];
+// console.log(cars);
+// cars.push('vW')
+// console.log(cars);
+// console.log(cars);
+​
+cars = ['vW']
+console.log(cars); */
+​
+//splice metodu
+/*
+var names = ["John", "Edward", "Victor"]
+// var deleted = names.splice (1,2, "Mark", "James")
+​
+// console.log('deleted', deleted);
+// console.log('names',names);
+​
+var deleted = names.splice (1,0, "Mark", "James")
+console.log('deleted', deleted);
+console.log('names',names); */
+​
+/* //slice metodu
+​
+const months = ['Jan', 'March', 'April', 'June',"july"];
+​
+​
+console.log(months.slice(1,3));
+console.log(months.slice(1,-2));
+console.log(months.slice(-4,-2));
+console.log(months.slice(-2,-4)); // []
+console.log(months);
+​
+console.log(months.reverse()); //son indexten başa doğru yazdırır orjiinali değiştirir
+console.log(months);
+ */
+​
+​
+//indexof & lastindexof
+​
+/* const colors = ["Red", "Yellow", "Green", "Blue", "Pink", "Green"];
+const x = colors.indexOf("Green", 3)
+const x = colors.indexOf("Gray") //returns -1
+console.log(x) */
+​
+/* const colors = ["Red", "Yellow", "Green", "Blue", "Green","Red","Yellow","Blue"];
+const last = colors.lastIndexOf("Blue", -2) // 6 ile de aynı sonucu verir
+const last1 = colors.lastIndexOf("Gray") //returns -1
+console.log(last1)
+ */
+​
